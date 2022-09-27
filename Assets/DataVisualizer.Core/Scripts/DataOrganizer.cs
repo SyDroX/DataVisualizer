bb017d50fa8af79ed7ab730b4b041a185ebfcc39
Kostya Sydoruk <sydoruk.kostya@gmail.com>
2022-09-27 14:40:30 +0300

    Added OrganizerData function, using the default layer order for now, added Node class, added GetUniqueDataGroups

--- Assets/DataVisualizer.Core/Scripts/DataOrganizer.cs
+++ Assets/DataVisualizer.Core/Scripts/DataOrganizer.cs
@@ -1,9 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace DataVisualizer.Core.Scripts
 {
+    public class Node
+    {
+        public string Name;
+        public List<Node> Children;
+    }
+    
     public class DataOrganizer : MonoBehaviour
     {
+        [SerializeField] private CsvDataReader _csvDataReader;
+
+        private void Start()
+        {
+            OrganizerData();
+        }
         
+        private async void OrganizerData()
+        {
+            Dictionary<string, List<string>> dataGroups = await _csvDataReader.GetData();
+            Dictionary<string, List<string>> uniqueDataGroups = GetUniqueDataGroups(dataGroups);
+            
+            // Default layers for now
+            string[] layers = dataGroups.Keys.ToArray();
+        }
+
+        private Dictionary<string, List<string>> GetUniqueDataGroups(Dictionary<string, List<string>> dataGroups)
+        {
+            Dictionary<string, List<string>> uniqueDataGroups = new Dictionary<string, List<string>>();
+            
+            foreach (string groupName in dataGroups.Keys)
+            {
+                uniqueDataGroups.Add(groupName, dataGroups[groupName].Distinct().ToList());
+            }
+
+            return uniqueDataGroups;
+        }
     }
 }
