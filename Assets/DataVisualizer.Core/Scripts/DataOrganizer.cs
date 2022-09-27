514b02ffee46eb87a23284c2f1c1e215e0fb93d0
Kostya Sydoruk <sydoruk.kostya@gmail.com>
2022-09-27 19:37:31 +0300

    Updated Node Data structure, Implemented a Node base solution to hold the names of the nodes by sorted layers

--- Assets/DataVisualizer.Core/Scripts/DataOrganizer.cs
+++ Assets/DataVisualizer.Core/Scripts/DataOrganizer.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 namespace DataVisualizer.Core.Scripts
 {
-    public class Node
+    public class Node 
     {
-        public string Name;
-        public List<Node> Children;
+        public Dictionary<string, Node> Children = new Dictionary<string, Node>();
     }
     
     public class DataOrganizer : MonoBehaviour
@@ -24,8 +23,47 @@ namespace DataVisualizer.Core.Scripts
             Dictionary<string, List<string>> dataGroups = await _csvDataReader.GetData();
             Dictionary<string, List<string>> uniqueDataGroups = GetUniqueDataGroups(dataGroups);
             
+            // All data groups lengths are equal to the number of rows extracted from the CSV
+            int rows = dataGroups.First().Value.Count;
+            
             // Default layers for now
             string[] layers = dataGroups.Keys.ToArray();
+            
+            Dictionary<string, Node> rootNodes2 = new Dictionary<string, Node>();
+            
+            foreach (string dataGroup in uniqueDataGroups[layers[0]])
+            {
+                rootNodes2.Add(dataGroup, new Node());
+            }
+            
+            for (int j = 0; j < rows; j++)
+            {
+                string currentRootName = dataGroups[layers[0]][j];
+                Node currentRootNode = rootNodes2[currentRootName];
+                
+                for (int i = 1; i < layers.Length; i++)
+                {
+                    bool hasChild = false;
+                    string currentNodeName = dataGroups[layers[i]][j];
+                    
+                    foreach (string childNode in currentRootNode.Children.Keys)
+                    {
+                        string parentNodeName = dataGroups[layers[i - 1]][j];
+                        
+                        if (childNode == currentNodeName && currentRootName == parentNodeName)
+                        {
+                            hasChild = true;
+                        }
+                    }
+
+                    if (!hasChild)
+                    {
+                        currentRootNode.Children.Add(dataGroups[layers[i]][j], new Node());
+                    }
+                    
+                    currentRootNode = currentRootNode.Children[currentNodeName];
+                }
+            }
         }
 
         private Dictionary<string, List<string>> GetUniqueDataGroups(Dictionary<string, List<string>> dataGroups)
