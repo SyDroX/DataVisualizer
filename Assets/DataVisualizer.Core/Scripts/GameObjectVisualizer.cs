4233702eb6518b8a3179491790769a7e927295b2
Kostya Sydoruk <sydoruk.kostya@gmail.com>
2022-09-28 13:29:50 +0300

    Added gameobject visualizer script

---
+++ Assets/DataVisualizer.Core/Scripts/GameObjectVisualizer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DataVisualizer.Core.Scripts
+{
+    public class GameObjectVisualizer : MonoBehaviour
+    {
+        [SerializeField] private GameObject _visualizerPrefab;
+        [SerializeField] private DataOrganizer _dataOrganizer;
+
+        private void Start()
+        {
+            
+        }
+    }
+}
