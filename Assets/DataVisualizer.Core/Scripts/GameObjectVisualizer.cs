0937417ec6bf70b041a689645fc60d9a333f2554
Kostya Sydoruk <sydoruk.kostya@gmail.com>
2022-09-28 18:31:48 +0300

    Added gameobjects references member for root nodes, added destory function, added listener to layers reorder

--- Assets/DataVisualizer.Core/Scripts/GameObjectVisualizer.cs
+++ Assets/DataVisualizer.Core/Scripts/GameObjectVisualizer.cs
@@ -5,32 +5,47 @@ namespace DataVisualizer.Core.Scripts
 {
     public class GameObjectVisualizer : MonoBehaviour
     {
+        private List<GameObject> _rootNodes = new List<GameObject>();
+
         [SerializeField] private GameObject _visualizerPrefab;
         [SerializeField] private DataOrganizer _dataOrganizer;
         [SerializeField] private GameObject _visualizationRoot;
-
+        
         private void Start()
         {
             VisualizeData();
+            _dataOrganizer.onLayersReordered.AddListener(VisualizeData);
         }
 
         private async void VisualizeData()
         {
+            DestroyVisualization();
             Dictionary<string, Node> organizedData = await _dataOrganizer.GetOrganizedData();
                     
             foreach (string nodeName in organizedData.Keys)
             {
                 GameObject nodeInstance = CreateNodeGameObject(nodeName, _visualizationRoot.transform);
+                _rootNodes.Add(nodeInstance);
                 VisualizeNode(organizedData[nodeName], nodeInstance);
             }
         }
 
+        private void DestroyVisualization()
+        {
+            foreach (GameObject rootNode in _rootNodes)
+            {
+                Destroy(rootNode);
+            }
+            
+            _rootNodes.Clear();
+        }
+        
         private GameObject CreateNodeGameObject(string nodeName, Transform parent)
         {
             GameObject nodeInstance = Instantiate(_visualizerPrefab, parent);
             nodeInstance.name = nodeName;
-            TextChanger textChanger = nodeInstance.GetComponent<TextChanger>();
-            textChanger.ChangeText(nodeName);
+            //TextChanger textChanger = nodeInstance.GetComponent<TextChanger>();
+            //textChanger.ChangeText(nodeName);
 
             return nodeInstance;
         }
