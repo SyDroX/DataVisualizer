67d50545c1543fe3b05499dc13bb59524f2b01be
Kostya Sydoruk <sydoruk.kostya@gmail.com>
2022-09-28 14:45:16 +0300

    Added visualization logic

--- Assets/DataVisualizer.Core/Scripts/GameObjectVisualizer.cs
+++ Assets/DataVisualizer.Core/Scripts/GameObjectVisualizer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace DataVisualizer.Core.Scripts
 {
@@ -6,10 +7,51 @@ namespace DataVisualizer.Core.Scripts
     {
         [SerializeField] private GameObject _visualizerPrefab;
         [SerializeField] private DataOrganizer _dataOrganizer;
+        [SerializeField] private GameObject _visualizationRoot;
 
         private void Start()
         {
-            
+            VisualizeData();
         }
+
+        private async void VisualizeData()
+        {
+            Dictionary<string, Node> organizedData = await _dataOrganizer.GetOrganizedData();
+                    
+            foreach (string nodeName in organizedData.Keys)
+            {
+                GameObject nodeInstance = CreateNodeGameObject(nodeName, _visualizationRoot.transform);
+
+                foreach (string childNodeName in organizedData[nodeName].Children.Keys)
+                {
+                    GameObject childNodeInstance = CreateNodeGameObject(childNodeName, nodeInstance.transform);
+                    VisualizeNode(organizedData[nodeName].Children[childNodeName], childNodeInstance);
+                }
+            }
+        }
+
+        private GameObject CreateNodeGameObject(string nodeName, Transform parent)
+        {
+            GameObject nodeInstance = Instantiate(_visualizerPrefab, parent);
+            nodeInstance.name = nodeName;
+            TextChanger textChanger = nodeInstance.GetComponent<TextChanger>();
+            textChanger.ChangeText(nodeName);
+
+            return nodeInstance;
+        }
+        
+        private void VisualizeNode(Node rootNode, GameObject rootGameObject)
+        {
+            foreach (string childNodeName in rootNode.Children.Keys)
+            {
+                GameObject nodeInstance = CreateNodeGameObject(childNodeName, rootGameObject.transform);
+                
+                foreach (Node childNode in rootNode.Children.Values)
+                {
+                    VisualizeNode(childNode, nodeInstance);
+                }
+            }
+        }
+        
     }
 }
